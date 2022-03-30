3e9c89c5d4d6490786f3d67a5e1e31b6188d53ce
Jason Bell <jason.jbell@yahoo.com>
2022-03-30 15:22:06 -0700

    added test button

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Button from '@mui/material/Button'
+
+const testy = () => {
+  console.log('pushed button')
+}
+
 
 const Home = () => {
   return (
     <div>
       <h1>BIG HOME</h1>
       <Link to={'/test'}>TESAT</Link>
+      <Button variant='contained' onClick={testy}>CHECK ME OUT</Button>
     </div>
   )
 }
