ee676da55c5b197f871d0840c8c069aa23c6951a
Jason Bell <jason.jbell@yahoo.com>
2022-03-29 17:21:16 -0700

    couple working test components

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <h1>BIG HOME</h1>
+    <div>
+      <h1>BIG HOME</h1>
+      <Link to={'/test'}>TESAT</Link>
+    </div>
   )
 }
 
