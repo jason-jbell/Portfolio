3dc215f46848337155a4ea794b39acf04c4705c7
Jason Bell <jason.jbell@yahoo.com>
2022-04-10 14:33:20 -0700

    removed Test.js, added redux

--- src/client/App.js
+++ src/client/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
-import Test from './components/Test'
+import SingleProject from './components/SingleProject'
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path='/test' element={<Test /> } />
+        <Route exact path='/projects/:id' element={<SingleProject /> } />
       </Routes>
     </BrowserRouter>
   )
